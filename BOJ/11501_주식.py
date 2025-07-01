00304108ec2723a97c81ef836cc7b3db82034d6c
Hanjise0ng <tjdwl3269@gmail.com>
2025-07-01 21:11:45 +0900

    greedy sliver 2

---
+++ BOJ/11501_주식.py
@@ -0,0 +1,20 @@
+# https://www.acmicpc.net/problem/11501
+# greedy sliver 2
+
+T = int(input())
+
+for t in range(T):
+  n = int(input())
+  arr = list(map(int, input().split()))
+  
+  revArr = arr[::-1]
+  cost = revArr[0]
+  ans = 0
+
+  for i in range(1, len(revArr)):
+    if cost < revArr[i]:
+      cost = revArr[i]
+    else:
+      ans += (cost - revArr[i])
+
+  print(ans)
