8349d0a2c723a14be9125f1d52a13a77b215ef15
Hanjise0ng <tjdwl3269@gmail.com>
2025-08-09 21:57:06 +0900

    recursion sliver 4

---
+++ BOJ/10994_별 찍기-19.py
@@ -0,0 +1,25 @@
+# https://www.acmicpc.net/problem/10994
+# recursion sliver 4
+
+N = int(input())
+stars = [[' ' for _ in range(4 * N - 3)] for _ in range(4 * N - 3)]
+
+def draw(n, x, y):
+
+    if n == 1:
+        stars[y][x] = "*"
+        return
+
+    length = 4 * n - 3
+    for i in range(length):
+        stars[y][x + i] = "*"
+        stars[y + i][x] = "*"
+        stars[y + length - 1][x + i] = "*"
+        stars[y + i][x + length - 1] = "*"
+
+    draw(n - 1, x + 2, y + 2)
+
+draw(N, 0, 0)
+
+for s in stars:
+    print("".join(s))
