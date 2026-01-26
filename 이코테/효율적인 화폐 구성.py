ea1507a29d40491bdd875f44fda1f1775632d8ee
Hanjise0ng <tjdwl3269@gmail.com>
2026-01-26 16:37:00 +0900

    level 2

--- 이코테/효율적인 화폐 구성.py
+++ 이코테/효율적인 화폐 구성.py
@@ -6,10 +6,10 @@ for i in range(n):
 d = [10001] * (m + 1)
 
 d[0] = 0
-for i in range(n):
-    for j in range(array[i], m + 1):
-        if d[j - array[i]] != 10001:
-            d[j] = min(d[j], d[j - array[i]] + 1)
+for i in range(n):                                # 1. 가지고 있는 화폐 종류만큼 반복
+    for j in range(array[i], m + 1):              # 2. 해당 화폐(k)로 만들 수 있는 금액부터 끝까지 반복
+        if d[j - array[i]] != 10001:              # 3. (j-k)원을 만드는 방법이 존재한다면
+            d[j] = min(d[j], d[j - array[i]] + 1) # 4. 더 작은 값으로 갱신
 
 if d[m] == 10001:
     print(-1)
