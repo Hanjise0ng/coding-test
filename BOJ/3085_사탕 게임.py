22d49e8a82ba9e1c5fb5fa51785a380ac0d5730e
Hanjise0ng <tjdwl3269@gmail.com>
2026-02-26 16:45:55 +0900

    bruteforcing silver 2

--- BOJ/3085_사탕 게임.py
+++ BOJ/3085_사탕 게임.py
@@ -1,36 +1,46 @@
 # https://www.acmicpc.net/problem/3085
 # bruteforcing silver 2
 
-def count(lst):
-    cnt = ans = 1
-
-    for i in range(1, len(lst)):
-        if lst[i] == lst[i-1]:
-            cnt += 1
-            ans = max(ans, cnt)
-        else:
-            cnt = 1
-
-    return ans
-
-def solve(arr):
-    ans = 0
-
-    for i in range(n-1):
-        for j in range(n):
-            # 오른쪽 사탕과 교환
-            arr[i][j], arr[i][j+1] = arr[i][j+1], arr[i][j]
-            ans = max(ans, count(arr[i]))
-            arr[i][j], arr[i][j+1] = arr[i][j+1], arr[i][j]
-
-            # 아래쪽 사탕과 교환
-            arr[i][j], arr[i+1][j] = arr[i+1][j], arr[i][j]
-            ans = max(ans, count(arr[i]), count(arr[i+1]))
-            arr[i][j], arr[i+1][j] = arr[i+1][j], arr[i][j]
-
+def count(rows, cols):
+    ans = 1
+    
+    for i in rows:
+        cnt = 1
+        for j in range(1, n):
+            if candy[i][j] == candy[i][j-1]:
+                cnt += 1
+                ans = max(ans, cnt)
+            else:
+                cnt = 1
+                
+    for j in cols:
+        cnt = 1
+        for i in range(1, n):
+            if candy[i][j] == candy[i-1][j]:
+                cnt += 1
+                ans = max(ans, cnt)
+            else:
+                cnt = 1
+                
     return ans
 
 n = int(input())
-candy = [list(input())+[0] for _ in range(n)] + [[0] * (n+1)]
-candy_t = list(map(list, zip(*candy)))
-print(max(solve(candy), solve(candy_t)))
+candy = [list(input().strip()) for _ in range(n)]
+
+ans = count(range(n), range(n))
+
+for i in range(n):
+    for j in range(n):
+        # 오른쪽 사탕과 교환
+        if j + 1 < n and candy[i][j] != candy[i][j+1]:
+            candy[i][j], candy[i][j+1] = candy[i][j+1], candy[i][j]
+            ans = max(ans, count([i], [j, j+1]))
+            candy[i][j], candy[i][j+1] = candy[i][j+1], candy[i][j]
+            
+        # 아래쪽 사탕과 교환
+        if i + 1 < n and candy[i][j] != candy[i+1][j]:
+            candy[i][j], candy[i+1][j] = candy[i+1][j], candy[i][j]
+            ans = max(ans, count([i, i+1], [j]))
+            candy[i][j], candy[i+1][j] = candy[i+1][j], candy[i][j]
+
+print(ans)
