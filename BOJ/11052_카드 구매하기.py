607401e6c5f44b07d1b98dcb63f260f9fe3e8d45
Hanjise0ng <tjdwl3269@gmail.com>
2026-02-27 16:07:56 +0900

    dp silver 1

---
+++ BOJ/11052_카드 구매하기.py
@@ -0,0 +1,11 @@
+# https://www.acmicpc.net/problem/11052
+# dp silver 1
+
+n = int(input())
+dp = [0] + list(map(int, input().split()))
+
+for i in range(1, n + 1):
+    for j in range(i):
+        dp[i] = max(dp[i], dp[j] + dp[i-j])
+
+print(max(dp))
