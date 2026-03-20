51c74e1e11151974beba51d7ed8bfc3332a77a40
Hanjise0ng <tjdwl3269@gmail.com>
2026-03-20 21:38:06 +0900

    math silver 2

---
+++ BOJ/2942_퍼거슨과 사과.py
@@ -0,0 +1,15 @@
+# https://www.acmicpc.net/problem/2942
+# math silver 2
+
+r, g = map(int, input().split())
+
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+n = gcd(r, g)
+
+for i in range(1, n + 1):
+    if n % i == 0:
+        print(i, r // i, g // i)
